0ee9d66cb2027113f956a7fa2942c962570acf32
aaron <aaronparfitt123@gmail.com>
2019-10-20 19:58:25 +0100

    Making progress

--- views/views.go
+++ views/views.go
@@ -8,9 +8,17 @@ import (
 
 
 func MainPage(w http.ResponseWriter, r *http.Request) {
+
+	switch r.Method{
+	case "GET":
 	t, err := template.ParseFiles("templates/index.html")
 	if err != nil {
 		fmt.Println(err)
 	}
 	t.Execute(w,nil)
+	case "POST":
+		fmt.Println("POST")
+
+	}
+	
 }

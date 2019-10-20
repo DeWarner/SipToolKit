f5fa98eddf2608b49ab04a93e0862ff43ce4e029
aaron <aaronparfitt123@gmail.com>
2019-10-20 20:34:52 +0100

    Adding docs in app

--- views/views.go
+++ views/views.go
@@ -10,12 +10,14 @@ import (
 func MainPage(w http.ResponseWriter, r *http.Request) {
 
 	switch r.Method{
+
 	case "GET":
-	t, err := template.ParseFiles("templates/index.html")
-	if err != nil {
-		fmt.Println(err)
-	}
-	t.Execute(w,nil)
+	    t, err := template.ParseFiles("templates/index.html")
+	    if err != nil {
+	    	fmt.Println(err)
+	    }
+		t.Execute(w,nil)
+		
 	case "POST":
 		fmt.Println("POST")
 
